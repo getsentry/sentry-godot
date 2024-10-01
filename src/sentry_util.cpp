0b0cbc58156d2f6dd95259958d9e0f8285419562
Serhii Snitsaruk <limbonaut@pm.me>
2024-10-01 19:02:23 +0200

    Improve marshalling between godot and sentry-native types

--- src/sentry_util.cpp
+++ src/sentry_util.cpp
@@ -4,17 +4,20 @@ using namespace godot;
 
 sentry_value_t SentryUtil::variant_to_sentry_value(const godot::Variant &p_variant) {
 	switch (p_variant.get_type()) {
+		case Variant::Type::NIL: {
+			return sentry_value_new_null();
+		} break;
 		case Variant::Type::BOOL: {
 			return sentry_value_new_bool(p_variant);
 		} break;
 		case Variant::Type::INT: {
 			return sentry_value_new_int32(p_variant);
 		} break;
-		case Variant::Type::STRING: {
-			return sentry_value_new_string(((String)p_variant).utf8());
-		} break;
 		case Variant::Type::FLOAT: {
 			return sentry_value_new_double(p_variant);
+		} break;
+		case Variant::Type::STRING: {
+			return sentry_value_new_string(((String)p_variant).utf8());
 		} break;
 		case Variant::Type::DICTIONARY: {
 			Dictionary dic = p_variant;
@@ -26,17 +29,30 @@ sentry_value_t SentryUtil::variant_to_sentry_value(const godot::Variant &p_varia
 			}
 			return sentry_dic;
 		} break;
-		case Variant::Type::ARRAY: {
-			Array arr = p_variant;
+		case Variant::Type::ARRAY:
+		case Variant::Type::PACKED_BYTE_ARRAY:
+		case Variant::Type::PACKED_INT32_ARRAY:
+		case Variant::Type::PACKED_INT64_ARRAY:
+		case Variant::Type::PACKED_FLOAT32_ARRAY:
+		case Variant::Type::PACKED_FLOAT64_ARRAY:
+		case Variant::Type::PACKED_STRING_ARRAY:
+		case Variant::Type::PACKED_VECTOR2_ARRAY:
+		case Variant::Type::PACKED_VECTOR3_ARRAY:
+		case Variant::Type::PACKED_COLOR_ARRAY:
+		case Variant::Type::PACKED_VECTOR4_ARRAY: {
+			bool oob = false;
+			bool valid = true;
+			int i = 0;
 			sentry_value_t sentry_list = sentry_value_new_list();
-			for (int i = 0; i < arr.size(); i++) {
-				sentry_value_append(sentry_list, variant_to_sentry_value(arr[i]));
-			}
+			do {
+				Variant item = p_variant.get_indexed(i++, valid, oob);
+				if (valid) {
+					sentry_value_append(sentry_list, variant_to_sentry_value(item));
+				}
+			} while (!oob);
 			return sentry_list;
 		} break;
-		case Variant::Type::NIL: {
-			return sentry_value_new_null();
-		} break;
+
 		default: {
 			return sentry_value_new_string(p_variant.stringify().utf8());
 		} break;
