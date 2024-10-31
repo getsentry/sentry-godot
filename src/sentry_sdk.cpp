2c7314a12871bc398434de917b064ad15f109f9e
Serhii Snitsaruk <limbonaut@pm.me>
2024-10-31 12:19:28 +0100

    Restore app context

--- src/sentry_sdk.cpp
+++ src/sentry_sdk.cpp
@@ -90,35 +90,30 @@ void SentrySDK::add_device_context() {
 	internal_sdk->set_context("device", device_context);
 }
 
-// void SentrySDK::add_app_context() {
-// 	ERR_FAIL_NULL(Time::get_singleton());
-// 	ERR_FAIL_NULL(ProjectSettings::get_singleton());
-// 	ERR_FAIL_NULL(Engine::get_singleton());
+void SentrySDK::add_app_context() {
+	ERR_FAIL_NULL(Time::get_singleton());
+	ERR_FAIL_NULL(ProjectSettings::get_singleton());
+	ERR_FAIL_NULL(Engine::get_singleton());
 
-// 	sentry_value_t app_context = sentry_value_new_object();
+	Dictionary app_context = Dictionary();
 
-// 	// app_start_time
-// 	double time = Time::get_singleton()->get_unix_time_from_system();
-// 	time -= Time::get_singleton()->get_ticks_msec() * 0.001;
-// 	String start_time = Time::get_singleton()->get_datetime_string_from_unix_time(time, true);
-// 	sentry_value_set_by_key(app_context, "app_start_time", sentry_value_new_string(start_time.utf8()));
+	double time = Time::get_singleton()->get_unix_time_from_system();
+	time -= Time::get_singleton()->get_ticks_msec() * 0.001;
+	String start_time = Time::get_singleton()->get_datetime_string_from_unix_time(time, true);
+	app_context["app_start_time"] = start_time;
 
-// 	// app_name
-// 	String app_name = ProjectSettings::get_singleton()->get_setting("application/config/name", "Unnamed project");
-// 	sentry_value_set_by_key(app_context, "app_name", sentry_value_new_string(app_name.utf8()));
+	String app_name = ProjectSettings::get_singleton()->get_setting("application/config/name", "Unnamed project");
+	app_context["app_name"] = app_name;
 
-// 	// app_version
-// 	String app_version = ProjectSettings::get_singleton()->get_setting("application/config/version", "");
-// 	if (!app_version.is_empty()) {
-// 		sentry_value_set_by_key(app_context, "app_version", sentry_value_new_string(app_version.utf8()));
-// 	}
+	String app_version = ProjectSettings::get_singleton()->get_setting("application/config/version", "");
+	if (!app_version.is_empty()) {
+		app_context["app_version"] = app_version;
+	}
 
-// 	// app_arch
-// 	String app_arch = Engine::get_singleton()->get_architecture_name();
-// 	sentry_value_set_by_key(app_context, "app_arch", sentry_value_new_string(app_arch.utf8()));
+	app_context["app_arch"] = Engine::get_singleton()->get_architecture_name();
 
-// 	sentry_set_context("app", app_context);
-// }
+	internal_sdk->set_context("app", app_context);
+}
 
 // void SentrySDK::add_gpu_context() {
 // 	ERR_FAIL_NULL(RenderingServer::get_singleton());
