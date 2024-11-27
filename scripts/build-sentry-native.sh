94088056a785a527be14ab651b34a12707af813c
Serhii Snitsaruk <limbonaut@pm.me>
2024-11-27 20:06:05 +0100

    chore: Move `sentry-native` to `modules/` directory and bump ref

--- scripts/build-sentry-native.sh
+++ scripts/build-sentry-native.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd sentry-native
+cd modules/sentry-native
 cmake -B build -DSENTRY_BUILD_SHARED_LIBS=OFF -DSENTRY_BACKEND=crashpad -DSENTRY_SDK_NAME="sentry.native.godot" -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build build --target sentry --parallel --config RelWithDebInfo
 cmake --build build --target crashpad_handler --parallel --config RelWithDebInfo
 cmake --install build --prefix install --config RelWithDebInfo
-cd ..
+cd ../..
