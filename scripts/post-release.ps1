9c6751c824f0742266b1c57c30ebb6486c166d67
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-10 22:15:00 +0100

    ci: Add post-release script

---
+++ scripts/post-release.ps1
@@ -0,0 +1,41 @@
+Param(
+    [Parameter(Mandatory = $true)][String]$prevVersion,
+    [Parameter(Mandatory = $true)][String]$newVersion
+)
+Set-StrictMode -Version latest
+
+git checkout main
+
+if ($newVersion -match '^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(?<status>.*)?$') {
+    $major = [int]$matches['major']
+    $minor = [int]$matches['minor']
+    $patch = [int]$matches['patch']
+    $status = $matches['status']
+
+    if ($status -match "^-(?<prereleaseName>[a-zA-Z]+)\.?(?<prereleaseVersion>\d+)?$") {
+        # Increment prerelease version
+        $prereleaseName = $matches['prereleaseName']
+        $prereleaseVersion = [int]$matches['prereleaseVersion']
+        $prereleaseVersion += 1
+        $status = "-$prereleaseName.$prereleaseVersion"
+    } else {
+        # Increment minor version, reset patch version, and add -dev prerelease status
+        $minor += 1
+        $patch = 0
+        $status = "-dev"
+    }
+
+    $nextVersion = "$major.$minor.$patch$status"
+
+    & ".\scripts\bump-version.ps1" -prevVersion $newVersion -newVersion $nextVersion
+
+    git diff --quiet
+    if ($LASTEXITCODE -ne 0) {
+        git commit -anm 'meta: Bump new development version'
+        git pull --rebase
+        git push
+    }
+
+} else {`
+    Throw "Failed to find version in $sconsFile"
+}
