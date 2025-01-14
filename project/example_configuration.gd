e9f6355b71b9e45127ac47ff83d07207973627e0
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-14 23:40:04 +0100

    Clarify comments

--- project/example_configuration.gd
+++ project/example_configuration.gd
@@ -1,18 +1,19 @@
 extends SentryConfiguration
 
-## Sets up filtering
+## Initialize filtering
 func _initialize() -> void:
+	# Set up before_send and on_crash callbacks
 	SentrySDK.set_before_send(_before_send)
 	SentrySDK.set_on_crash(_on_crash)
 
 
-## before_send example
+## before_send callback example
 func _before_send(ev: SentryEvent) -> SentryEvent:
 	print("Processing event: ", ev.id)
 	return ev
 
 
-## on_crash example
+## on_crash callback example
 func _on_crash(ev: SentryEvent) -> SentryEvent:
 	print("Crashing with event: ", ev.id)
 	return ev
