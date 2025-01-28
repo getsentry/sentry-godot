46c122e68a1a58aee65477ec761adbea4c912433
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-28 17:30:56 +0100

    Expand configuration example

--- project/example_configuration.gd
+++ project/example_configuration.gd
@@ -7,7 +7,9 @@ extends SentryConfiguration
 ## Configure Sentry SDK options
 func _configure(options: SentryOptions) -> void:
 	print("[example_configuration.gd] Configuring SDK options via GDScript")
+
 	options.debug = true
+	options.release = "sentry-godot-demo@" + ProjectSettings.get_setting("application/config/version")
 
 	# Set up event callbacks
 	options.before_send = _before_send
