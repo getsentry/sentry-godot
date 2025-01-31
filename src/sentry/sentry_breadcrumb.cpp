b236071c570217a5b85a71a6e1585a6750d5ab59
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-31 16:44:10 +0100

    Add SentryBreadcrumb abstract class

---
+++ src/sentry/sentry_breadcrumb.cpp
@@ -0,0 +1,12 @@
+#include "sentry_breadcrumb.h"
+
+#include "sentry/simple_bind.h"
+#include "sentry_sdk.h" // Needed for VariantCaster<SentrySDK::Level>
+
+void SentryBreadcrumb::_bind_methods() {
+	BIND_PROPERTY_SIMPLE(SentryBreadcrumb, Variant::STRING, message);
+	BIND_PROPERTY_SIMPLE(SentryBreadcrumb, Variant::STRING, category);
+	BIND_PROPERTY(SentryBreadcrumb, sentry::make_level_enum_property("level"), set_level, get_level);
+	BIND_PROPERTY_SIMPLE(SentryBreadcrumb, Variant::STRING, type);
+	BIND_PROPERTY_SIMPLE(SentryBreadcrumb, Variant::DICTIONARY, data);
+}
