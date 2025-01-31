ac3f7c385188c5b0a1c3c795b557c6735393afec
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-31 01:03:12 +0100

    Test SentrySDK.set_tag()

--- project/test/suites/test_sdk.gd
+++ project/test/suites/test_sdk.gd
@@ -1,9 +1,28 @@
 class_name TestSDK
 extends GdUnitTestSuite
 
+
+signal callback_processed
+
+
 ## SentrySDK.capture_message() should return a non-empty event ID, which must match the ID returned by the get_last_event_id() call.
 func test_capture_message() -> void:
 	var event_id := SentrySDK.capture_message("capture_message_test", SentrySDK.LEVEL_DEBUG)
 	assert_str(event_id).is_not_empty()
 	assert_str(SentrySDK.get_last_event_id()).is_not_empty()
 	assert_str(event_id).is_equal(SentrySDK.get_last_event_id())
+
+
+## SentrySDK.set_tag() should set the tag on the event object.
+func test_set_tag() -> void:
+	SentrySDK._set_before_send(
+		func(ev: SentryEvent):
+			assert_str(ev.get_tag("custom-tag")).is_equal("custom-tag-value")
+			callback_processed.emit()
+			return null)
+
+	SentrySDK.set_tag("custom-tag", "custom-tag-value")
+	SentrySDK.capture_message("test-tags")
+	assert_signal(self).is_emitted("callback_processed")
+
+	SentrySDK._unset_before_send()
