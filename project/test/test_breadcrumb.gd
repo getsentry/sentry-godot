7b272fd26f388be60ca4175c1146a5557fff6318
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-31 21:17:45 +0100

    Test before_breadcrumb callback

--- project/test/test_breadcrumb.gd
+++ project/test/test_breadcrumb.gd
@@ -2,6 +2,8 @@ extends GdUnitTestSuite
 ## Test SentryBreadcrumb class.
 
 
+signal callback_processed
+
 var crumb: SentryBreadcrumb
 
 
@@ -27,6 +29,27 @@ func test_breadcrumb_level() -> void:
 func test_breadcrumb_type() -> void:
     crumb.type = "test-type"
     assert_str(crumb.type).is_equal("test-type")
+
+
+func test_breadcrumb_callback() -> void:
+    SentrySDK._set_before_breadcrumb(
+        func(b: SentryBreadcrumb):
+            assert_str(b.message).is_equal("test-message")
+            assert_str(b.category).is_equal("test-category")
+            assert_int(b.level).is_equal(SentrySDK.LEVEL_DEBUG)
+            assert_str(b.type).is_equal("test-type")
+            # assert_dict(b.data).is_equal({"test": "data"})
+            callback_processed.emit()
+            return null)
+
+    crumb.message = "test-message"
+    crumb.category = "test-category"
+    crumb.level = SentrySDK.LEVEL_DEBUG
+    crumb.type = "test-type"
+    # crumb.data = {"test": "data"}
+    SentrySDK.capture_breadcrumb(crumb)
+
+    assert_signal(self).is_emitted("callback_processed")
 
 
 # TODO: implement data!
