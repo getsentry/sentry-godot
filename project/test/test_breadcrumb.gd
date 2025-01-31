ee9d823f2d7e21e95cc7dc55e63aa1b307aecac5
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-31 21:46:55 +0100

    spaces -> tabs

--- project/test/test_breadcrumb.gd
+++ project/test/test_breadcrumb.gd
@@ -8,48 +8,48 @@ var crumb: SentryBreadcrumb
 
 
 func before_test() -> void:
-    crumb = SentrySDK.create_breadcrumb()
+	crumb = SentrySDK.create_breadcrumb()
 
 
 func test_breadcrumb_message() -> void:
-    crumb.message = "test-message"
-    assert_str(crumb.message).is_equal("test-message")
+	crumb.message = "test-message"
+	assert_str(crumb.message).is_equal("test-message")
 
 
 func test_breadcrumb_category() -> void:
-    crumb.category = "test-category"
-    assert_str(crumb.category).is_equal("test-category")
+	crumb.category = "test-category"
+	assert_str(crumb.category).is_equal("test-category")
 
 
 func test_breadcrumb_level() -> void:
-    crumb.level = SentrySDK.LEVEL_DEBUG
-    assert_int(crumb.level).is_equal(SentrySDK.LEVEL_DEBUG)
+	crumb.level = SentrySDK.LEVEL_DEBUG
+	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_DEBUG)
 
 
 func test_breadcrumb_type() -> void:
-    crumb.type = "test-type"
-    assert_str(crumb.type).is_equal("test-type")
+	crumb.type = "test-type"
+	assert_str(crumb.type).is_equal("test-type")
 
 
 func test_breadcrumb_callback() -> void:
-    SentrySDK._set_before_breadcrumb(
-        func(b: SentryBreadcrumb):
-            assert_str(b.message).is_equal("test-message")
-            assert_str(b.category).is_equal("test-category")
-            assert_int(b.level).is_equal(SentrySDK.LEVEL_DEBUG)
-            assert_str(b.type).is_equal("test-type")
-            # assert_dict(b.data).is_equal({"test": "data"})
-            callback_processed.emit()
-            return null)
+	SentrySDK._set_before_breadcrumb(
+		func(b: SentryBreadcrumb):
+			assert_str(b.message).is_equal("test-message")
+			assert_str(b.category).is_equal("test-category")
+			assert_int(b.level).is_equal(SentrySDK.LEVEL_DEBUG)
+			assert_str(b.type).is_equal("test-type")
+			# assert_dict(b.data).is_equal({"test": "data"})
+			callback_processed.emit()
+			return null)
 
-    crumb.message = "test-message"
-    crumb.category = "test-category"
-    crumb.level = SentrySDK.LEVEL_DEBUG
-    crumb.type = "test-type"
-    # crumb.data = {"test": "data"}
-    SentrySDK.capture_breadcrumb(crumb)
+	crumb.message = "test-message"
+	crumb.category = "test-category"
+	crumb.level = SentrySDK.LEVEL_DEBUG
+	crumb.type = "test-type"
+	# crumb.data = {"test": "data"}
+	SentrySDK.capture_breadcrumb(crumb)
 
-    assert_signal(self).is_emitted("callback_processed")
+	assert_signal(self).is_emitted("callback_processed")
 
 
 # TODO: implement data!
