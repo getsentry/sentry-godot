554fc691840071e861101bed99fd920e67c9d19c
Serhii Snitsaruk <limbonaut@pm.me>
2025-01-31 18:22:47 +0100

    Implement NativeBreadcrumb

---
+++ src/sentry/native/native_breadcrumb.h
@@ -0,0 +1,35 @@
+#ifndef NATIVE_BREADCRUMB_H
+#define NATIVE_BREADCRUMB_H
+
+#include "sentry_breadcrumb.h"
+
+#include <sentry.h>
+
+class NativeBreadcrumb : public SentryBreadcrumb {
+	GDCLASS(NativeBreadcrumb, SentryBreadcrumb);
+
+private:
+	sentry_value_t native_crumb;
+
+public:
+	virtual void set_message(const String &p_message) override;
+	virtual String get_message() const override;
+
+	virtual void set_category(const String &p_category) override;
+	virtual String get_category() const override;
+
+	virtual void set_level(sentry::Level p_level) override;
+	virtual sentry::Level get_level() const override;
+
+	virtual void set_type(const String &p_type) override;
+	virtual String get_type() const override;
+
+	virtual void set_data(const Dictionary &p_data) override;
+	virtual Dictionary get_data() const override;
+
+	NativeBreadcrumb(sentry_value_t p_native_crumb);
+	NativeBreadcrumb();
+	virtual ~NativeBreadcrumb() override;
+};
+
+#endif // NATIVE_BREADCRUMB_H
