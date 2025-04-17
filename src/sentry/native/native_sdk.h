ba07fff9bb93dc9e93676e683a097b7684dbf885
Serhii Snitsaruk <limbonaut@pm.me>
2025-04-17 23:03:57 +0200

    ref: Remove `logger` argument from `SentrySDK.capture_message(...)`
    Seeing that it had a default value and seldomly used, it shouldn't be a disruptive
    change.

--- src/sentry/native/native_sdk.h
+++ src/sentry/native/native_sdk.h
@@ -26,7 +26,7 @@ public:
 	virtual void add_breadcrumb(const String &p_message, const String &p_category, Level p_level,
 			const String &p_type = "default", const Dictionary &p_data = Dictionary()) override;
 
-	virtual String capture_message(const String &p_message, Level p_level = sentry::LEVEL_INFO, const String &p_logger = "") override;
+	virtual String capture_message(const String &p_message, Level p_level = sentry::LEVEL_INFO) override;
 	virtual String get_last_event_id() override;
 	virtual String capture_error(const String &p_type, const String &p_value, Level p_level, const Vector<StackFrame> &p_frames) override;
 
