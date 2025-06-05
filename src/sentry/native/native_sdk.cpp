54f31a1149d976479d7b1eb7dec8f8be3a38ca33
Serhii Snitsaruk <limbonaut@pm.me>
2025-06-05 10:38:11 +0200

    Use non-greedy wildcard expansion

--- src/sentry/native/native_sdk.cpp
+++ src/sentry/native/native_sdk.cpp
@@ -226,7 +226,7 @@ void _log_native_message(sentry_level_t level, const char *message, va_list args
 
 	// Filter out warnings about missing attachment files that may not exist in some scenarios.
 	static auto pattern = std::regex{
-		R"(^failed to read envelope item from \".*(screenshot\.jpg|view-hierarchy\.json)\")"
+		R"(^failed to read envelope item from \".*?(screenshot\.jpg|view-hierarchy\.json)\")"
 	};
 	if (std::regex_search(buffer, buffer + required, pattern)) {
 		accepted = false;
