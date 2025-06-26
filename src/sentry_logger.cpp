230d2e896d87866ffb91b91a2201be42df68fa46
Serhii Snitsaruk <limbonaut@pm.me>
2025-06-26 12:50:52 +0200

    Fix SentryLogger::_log_error signature

--- src/sentry_logger.cpp
+++ src/sentry_logger.cpp
@@ -75,7 +75,7 @@ void SentryLogger::_process_frame() {
 
 void SentryLogger::_log_error(const String &p_function, const String &p_file, int32_t p_line,
 		const String &p_code, const String &p_rationale, bool p_editor_notify, int32_t p_error_type,
-		const TypedArray<ScriptBacktrace> &p_script_backtraces) {
+		const TypedArray<Ref<ScriptBacktrace>> &p_script_backtraces) {
 	SourceLine source_line{ p_file.utf8(), p_line };
 
 	error_mutex->lock();
