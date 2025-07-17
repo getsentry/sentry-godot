c81b502f761123502d50677eb7fd9102b4295eec
Serhii Snitsaruk <limbonaut@pm.me>
2025-07-17 21:29:39 +0200

    Ditch Objective-C wrapper

--- src/sentry/cocoa/cocoa_sdk.mm
+++ src/sentry/cocoa/cocoa_sdk.mm
@@ -4,58 +4,142 @@
 #include "sentry/processing/process_event.h"
 #include "sentry/sentry_attachment.h"
 #include "sentry/util/print.h"
-#include "sentry_objc_wrapper.h"
 
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/classes/project_settings.hpp>
 
+#include <Sentry/Sentry.h>
+
 using namespace godot;
+
+namespace objc {
+
+using SentryOptions = SentryOptions;
+using SentrySDK = SentrySDK;
+using SentryEvent = SentryEvent;
+using SentryUser = SentryUser;
+using SentryLevel = SentryLevel;
+using SentryBreadcrumb = SentryBreadcrumb;
+using SentryId = SentryId;
+using SentryScope = SentryScope;
+
+} // namespace objc
+
+namespace {
+
+objc::SentryLevel sentry_level_to_objc(sentry::Level p_level) {
+	switch (p_level) {
+		case sentry::Level::LEVEL_DEBUG:
+			return objc::SentryLevel(1);
+		case sentry::Level::LEVEL_INFO:
+			return objc::SentryLevel(2);
+		case sentry::Level::LEVEL_WARNING:
+			return objc::SentryLevel(3);
+		case sentry::Level::LEVEL_ERROR:
+			return objc::SentryLevel(4);
+		case sentry::Level::LEVEL_FATAL:
+			return objc::SentryLevel(5);
+		default:
+			return objc::SentryLevel(4);
+	}
+}
+
+} // unnamed namespace
 
 namespace sentry {
 
 void CocoaSDK::set_context(const String &p_key, const Dictionary &p_value) {
-	objc_wrapper::set_context_objc(p_key, p_value);
+	[objc::SentrySDK configureScope:^(objc::SentryScope *scope) {
+		NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+
+		Array keys = p_value.keys();
+		for (int i = 0; i < keys.size(); i++) {
+			Variant k = keys[i];
+			Variant v = p_value[k];
+
+			NSString *keyStr = [NSString stringWithUTF8String:k.stringify().utf8()];
+			// TODO: value has to be properly typed!
+			NSString *valueStr = [NSString stringWithUTF8String:v.stringify().utf8()];
+
+			[dict setObject:valueStr forKey:keyStr];
+		}
+
+		[scope setContextValue:dict forKey:[NSString stringWithUTF8String:p_key.utf8()]];
+	}];
 }
 
 void CocoaSDK::remove_context(const String &p_key) {
-	objc_wrapper::remove_context_objc(p_key);
+	[objc::SentrySDK configureScope:^(objc::SentryScope *scope) {
+		[scope removeContextForKey:[NSString stringWithUTF8String:p_key.utf8()]];
+	}];
 }
 
 void CocoaSDK::set_tag(const String &p_key, const String &p_value) {
-	objc_wrapper::set_tag_objc(p_key, p_value);
+	[objc::SentrySDK configureScope:^(objc::SentryScope *scope) {
+		[scope setTagValue:[NSString stringWithUTF8String:p_value.utf8()]
+					forKey:[NSString stringWithUTF8String:p_key.utf8()]];
+	}];
 }
 
 void CocoaSDK::remove_tag(const String &p_key) {
-	objc_wrapper::remove_tag_objc(p_key);
+	[objc::SentrySDK configureScope:^(objc::SentryScope *scope) {
+		[scope removeTagForKey:[NSString stringWithUTF8String:p_key.utf8()]];
+	}];
 }
 
 void CocoaSDK::set_user(const Ref<SentryUser> &p_user) {
 	if (p_user.is_valid()) {
-		objc_wrapper::set_user_objc(
-				p_user->get_id(),
-				p_user->get_username(),
-				p_user->get_email(),
-				p_user->get_ip_address());
+		objc::SentryUser *user = [[objc::SentryUser alloc] init];
+
+		if (!p_user->get_id().is_empty()) {
+			user.userId = [NSString stringWithUTF8String:p_user->get_id().utf8()];
+		}
+		if (!p_user->get_username().is_empty()) {
+			user.username = [NSString stringWithUTF8String:p_user->get_username().utf8()];
+		}
+		if (!p_user->get_email().is_empty()) {
+			user.email = [NSString stringWithUTF8String:p_user->get_email().utf8()];
+		}
+		if (!p_user->get_ip_address().is_empty()) {
+			user.ipAddress = [NSString stringWithUTF8String:p_user->get_ip_address().utf8()];
+		}
+
+		[objc::SentrySDK setUser:user];
 	} else {
-		objc_wrapper::remove_user_objc();
+		remove_user();
 	}
 }
 
 void CocoaSDK::remove_user() {
-	objc_wrapper::remove_user_objc();
+	[objc::SentrySDK setUser:nil];
 }
 
 void CocoaSDK::add_breadcrumb(const String &p_message, const String &p_category, Level p_level,
 		const String &p_type, const Dictionary &p_data) {
-	objc_wrapper::add_breadcrumb_objc(p_message, p_category, p_level, p_type);
+	objc::SentryBreadcrumb *breadcrumb = [[objc::SentryBreadcrumb alloc] init];
+	breadcrumb.message = [NSString stringWithUTF8String:p_message.utf8()];
+	breadcrumb.category = [NSString stringWithUTF8String:p_category.utf8()];
+	breadcrumb.type = [NSString stringWithUTF8String:p_type.utf8()];
+	breadcrumb.level = sentry_level_to_objc(p_level);
+
+	[objc::SentrySDK addBreadcrumb:breadcrumb];
 }
 
 String CocoaSDK::capture_message(const String &p_message, Level p_level) {
-	return objc_wrapper::capture_message_objc(p_message, p_level);
+	objc::SentryId *event_id = [objc::SentrySDK captureMessage:[NSString stringWithUTF8String:p_message.utf8()]
+												withScopeBlock:^(objc::SentryScope *scope) {
+													scope.level = sentry_level_to_objc(p_level);
+												}];
+
+	if (event_id) {
+		return String([[event_id description] UTF8String]);
+	}
+	return "";
 }
 
 String CocoaSDK::get_last_event_id() {
-	return objc_wrapper::get_last_event_id_objc();
+	// TODO: figure out what to do with this (maybe handle last id via callbacks).
+	return String();
 }
 
 Ref<SentryEvent> CocoaSDK::create_event() {
@@ -74,14 +158,20 @@ void CocoaSDK::add_attachment(const Ref<SentryAttachment> &p_attachment) {
 
 void CocoaSDK::initialize(const PackedStringArray &p_global_attachments) {
 	String dsn = SentryOptions::get_singleton()->get_dsn();
-	objc_wrapper::initialize_sentry_sdk(dsn, true);
+	bool debug_enabled = SentryOptions::get_singleton()->is_debug_enabled();
+
+	[objc::SentrySDK startWithConfigureOptions:^(objc::SentryOptions *options) {
+		options.dsn = [NSString stringWithUTF8String:dsn.utf8()];
+		options.debug = debug_enabled;
+	}];
+
 	initialized = true;
 }
 
 CocoaSDK::~CocoaSDK() {
 	if (initialized) {
-		objc_wrapper::shutdown_sentry_sdk();
+		[objc::SentrySDK close];
 	}
 }
 
-} //namespace sentry
+} // namespace sentry
