a53b1432f42446b134d6d0e7e408c7cc5307d90d
Serhii Snitsaruk <limbonaut@pm.me>
2025-09-01 12:16:09 +0200

    Update test_breadcrumb.gd

--- project/test/suites/test_breadcrumb.gd
+++ project/test/suites/test_breadcrumb.gd
@@ -2,20 +2,25 @@ extends GdUnitTestSuite
 ## Test SentryBreadcrumb class properties and methods.
 
 
+func test_breadcrumb_create_with_message() -> void:
+	var crumb := SentryBreadcrumb.create("test-message")
+	assert_str(crumb.message).is_equal("test-message")
+
+
 func test_breadcrumb_message() -> void:
-	var crumb: SentryBreadcrumb = SentrySDK.create_breadcrumb()
+	var crumb := SentryBreadcrumb.create()
 	crumb.message = "test-message"
 	assert_str(crumb.message).is_equal("test-message")
 
 
 func test_breadcrumb_category() -> void:
-	var crumb: SentryBreadcrumb = SentrySDK.create_breadcrumb()
+	var crumb := SentryBreadcrumb.create()
 	crumb.category = "test-category"
 	assert_str(crumb.category).is_equal("test-category")
 
 
 func test_breadcrumb_level() -> void:
-	var crumb: SentryBreadcrumb = SentrySDK.create_breadcrumb()
+	var crumb := SentryBreadcrumb.create()
 	crumb.level = SentrySDK.LEVEL_DEBUG
 	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_DEBUG)
 	crumb.level = SentrySDK.LEVEL_INFO
@@ -29,59 +34,19 @@ func test_breadcrumb_level() -> void:
 
 
 func test_breadcrumb_type() -> void:
-	var crumb: SentryBreadcrumb = SentrySDK.create_breadcrumb()
+	var crumb := SentryBreadcrumb.create()
 	crumb.type = "test-type"
 	assert_str(crumb.type).is_equal("test-type")
 
 
 func test_breadcrumb_can_set_data() -> void:
-	var crumb: SentryBreadcrumb = SentrySDK.create_breadcrumb()
+	var crumb := SentryBreadcrumb.create()
 	crumb.set_data({"biome": "forest", "time_of_day": 0.42})
 
 
 func test_breadcrumb_default_values() -> void:
-	var crumb: SentryBreadcrumb = SentrySDK.create_breadcrumb()
+	var crumb := SentryBreadcrumb.create()
 	assert_str(crumb.message).is_empty()
 	assert_str(crumb.type).is_empty()
 	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_INFO)
 	assert_bool(crumb.category in ["", "default"]).is_true()
-
-
-func test_breadcrumb_info_shortcut() -> void:
-	var crumb := SentryBreadcrumb.info("Info message")
-	assert_str(crumb.message).is_equal("Info message")
-	assert_str(crumb.type).is_equal("info")
-	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_INFO)
-	assert_bool(crumb.category in ["", "default"]).is_true()
-
-
-func test_breadcrumb_debug_shortcut() -> void:
-	var crumb := SentryBreadcrumb.debug("Debug message")
-	assert_str(crumb.message).is_equal("Debug message")
-	assert_str(crumb.type).is_equal("debug")
-	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_DEBUG)
-	assert_bool(crumb.category in ["", "default"]).is_true()
-
-
-func test_breadcrumb_error_shortcut() -> void:
-	var crumb := SentryBreadcrumb.error("Error happened")
-	assert_str(crumb.message).is_equal("Error happened")
-	assert_str(crumb.type).is_equal("error")
-	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_ERROR)
-	assert_bool(crumb.category in ["", "default"]).is_true()
-
-
-func test_breadcrumb_query_shortcut() -> void:
-	var crumb := SentryBreadcrumb.query("Query performed")
-	assert_str(crumb.message).is_equal("Query performed")
-	assert_str(crumb.type).is_equal("query")
-	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_INFO)
-	assert_bool(crumb.category in ["", "default"]).is_true()
-
-
-func test_breadcrumb_user_shortcut() -> void:
-	var crumb := SentryBreadcrumb.user("ui.click", "User clicked button")
-	assert_str(crumb.message).is_equal("User clicked button")
-	assert_str(crumb.type).is_equal("user")
-	assert_int(crumb.level).is_equal(SentrySDK.LEVEL_INFO)
-	assert_str(crumb.category).is_equal("ui.click")
