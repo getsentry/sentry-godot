979d88cf44f35738049a92ed13804320f464cbc7
Serhii Snitsaruk <limbonaut@pm.me>
2025-09-23 02:09:15 -0700

    chore: Demo: Add "Run tests" button instead of running tests on start (#379)

--- project/mobile.gd
+++ project/mobile.gd
@@ -1,11 +1,18 @@
 extends CanvasLayer
 
+const MOBILE_TESTS_FILE := "res://test/mobile_tests.gd"
+
+
 func _ready() -> void:
 	get_viewport().size = Vector2(810, 1530) # simulate mobile screen on desktop
 	get_viewport().get_window().content_scale_factor = 3.0
 	get_viewport().get_window().content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS
 	get_viewport().get_window().content_scale_aspect = Window.CONTENT_SCALE_ASPECT_EXPAND
 
-	if OS.get_name() == "Android":
-		var tests = load("res://test/test_on_android.gd").new()
-		tests.run_tests()
+	# Show tests only if the test file is available
+	%Tests.visible = FileAccess.file_exists(MOBILE_TESTS_FILE)
+
+
+func _on_run_tests_button_pressed() -> void:
+	var tests = load(MOBILE_TESTS_FILE).new()
+	tests.run_tests()
