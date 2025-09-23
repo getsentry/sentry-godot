6d5c9262186faae358d0b9d648f7132fb999927c
Serhii Snitsaruk <limbonaut@pm.me>
2025-09-23 16:56:19 +0200

    Add test

---
+++ project/test/isolated/test_limit_throttling_disabled.gd
@@ -0,0 +1,35 @@
+extends GdUnitTestSuite
+## Test throttling should be disabled if the window is set to 0.
+
+
+signal callback_processed
+
+var _num_events: int = 0
+
+
+func before() -> void:
+	SentrySDK.init(func(options: SentryOptions) -> void:
+		# Setting throttle window to 0 should disable throttling.
+		options.logger_limits.throttle_window_ms = 0
+		# Make sure other limits are not interfering.
+		options.logger_limits.events_per_frame = 88
+		options.before_send = _before_send
+	)
+
+
+func _before_send(_ev: SentryEvent) -> SentryEvent:
+	_num_events += 1
+	callback_processed.emit()
+	return null
+
+
+## All errors should be logged.
+func test_throttling_window_set_to_zero() -> void:
+	monitor_signals(self, false)
+	push_error("dummy-error 1")
+	push_error("dummy-error 2")
+	push_error("dummy-error 3")
+	push_error("dummy-error 4")
+	await assert_signal(self).is_emitted("callback_processed")
+	await get_tree().create_timer(0.1).timeout
+	assert_int(_num_events).is_equal(4)
