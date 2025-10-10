53c55730787904b08195593c45e463c5a1a8af16
Serhii Snitsaruk <limbonaut@pm.me>
2025-10-10 14:03:29 +0200

    Add UTF-8 test

--- project/test/isolated/test_structured_logs.gd
+++ project/test/isolated/test_structured_logs.gd
@@ -118,3 +118,14 @@ func test_structured_logs_with_attribute_removal() -> void:
 	SentrySDK.logger.info("Test 123", [], {
 		"level": "forest",
 	})
+
+
+func test_strucutured_logs_with_utf8() -> void:
+	log_processed.connect(func(entry: SentryLog):
+		assert_str(entry.body).is_equal("Hello 世界! 👋")
+		assert_str(entry.get_attribute("sentry.message.template")).is_equal("Hello %s! 👋")
+		assert_str(entry.get_attribute("sentry.message.parameter.0")).is_equal("世界")
+	, CONNECT_ONE_SHOT)
+	SentrySDK.logger.info("Hello %s! 👋", ["世界"], {
+		"world": "世界"
+	})
