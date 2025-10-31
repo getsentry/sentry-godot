38b64eec050af8ab36eb0acd39f2214d2dd7cd41
Serhii Snitsaruk <limbonaut@pm.me>
2025-10-31 10:00:11 +0100

    Extract formatting functions into Fmt class

--- project/test/util/test_runner.gd
+++ project/test/util/test_runner.gd
@@ -45,10 +45,10 @@ var _included_tests := PackedStringArray()
 
 ## Initialize test execution
 func init_runner() -> void:
-	print_rich(_step(), _prominent("Initializing test runner..."))
+	print_rich(Fmt.step(), Fmt.prominent("Initializing test runner..."))
 	_test_cases = _discover_tests()
 	if _test_cases.is_empty():
-		print_rich(_error("No test cases found!"))
+		print_rich(Fmt.error("No test cases found!"))
 		_finish(Result.TESTS_NOT_FOUND)
 		return
 	_state = RUN
@@ -84,9 +84,9 @@ func _discover_tests() -> Array[GdUnitTestCase]:
 	for path in _included_tests:
 		var scripts := scanner.scan(path)
 		for script in scripts:
-			print_rich(_step(), "Scanning: ", script.resource_path)
+			print_rich(Fmt.step(), "Scanning: ", Fmt.suite(script.resource_path))
 			GdUnitTestDiscoverer.discover_tests(script, func(test: GdUnitTestCase) -> void:
-				print_rich(_substep(), "Discovered %s" % test.display_name)
+				print_rich(Fmt.substep(), "Discovered %s" % Fmt.case(test.display_name))
 				_test_cases.append(test)
 				gdunit_test_discover_added.emit(test)
 			)
@@ -108,20 +108,20 @@ func _finish(code: int) -> void:
 func _on_gdunit_event(event: GdUnitEvent) -> void:
 	match event.type():
 		GdUnitEvent.INIT:
-			print_rich(_step(), _prominent("Initializing..."))
+			print_rich(Fmt.step(), Fmt.prominent("Initializing..."))
 			stats.clear()
 			suite_stats.clear()
 
 		GdUnitEvent.STOP:
-			print_rich(_step(), _prominent("Finished all tests."))
+			print_rich(Fmt.step(), Fmt.prominent("Finished all tests."))
 			_print_stats(stats, "Overall Summary")
 
 		GdUnitEvent.TESTSUITE_BEFORE:
-			print_rich(_step(), _prominent("Loading: "), _suite(event.resource_path()))
+			print_rich(Fmt.step(), Fmt.prominent("Loading: "), Fmt.suite(event.resource_path()))
 			suite_stats.clear()
 
 		GdUnitEvent.TESTSUITE_AFTER:
-			print_rich(_substep(), _prominent("Finished: "), _suite(event.resource_path()))
+			print_rich(Fmt.substep(), Fmt.prominent("Finished: "), Fmt.suite(event.resource_path()))
 			_print_failure_report(event.reports())
 			_print_stats(suite_stats, "Summary")
 			stats.num_total += suite_stats.num_total
@@ -133,8 +133,8 @@ func _on_gdunit_event(event: GdUnitEvent) -> void:
 
 		GdUnitEvent.TESTCASE_BEFORE:
 			var test := _test_session.find_test_by_id(event.guid())
-			print_rich(_substep(), _prominent("Started: "),
-					_suite(test.suite_name), " > ", _case(test.display_name))
+			print_rich(Fmt.substep(), Fmt.prominent("Started: "),
+					Fmt.suite(test.suite_name), " > ", Fmt.case(test.display_name))
 
 		GdUnitEvent.TESTCASE_AFTER:
 			suite_stats.num_total += 1
@@ -146,45 +146,22 @@ func _on_gdunit_event(event: GdUnitEvent) -> void:
 
 			var test := _test_session.find_test_by_id(event.guid())
 			if event.is_success():
-				_print_result(_bold(_success("PASSED")), test.suite_name, test.display_name)
+				_print_result(Fmt.bold(Fmt.success("PASSED")), test.suite_name, test.display_name)
 			elif event.is_skipped():
-				_print_result(_bold(_muted("SKIPPED")), test.suite_name, test.display_name)
+				_print_result(Fmt.bold(Fmt.muted("SKIPPED")), test.suite_name, test.display_name)
 			elif event.is_failed() or event.is_error():
-				_print_result(_bold(_error("FAILED")), test.suite_name, test.display_name)
+				_print_result(Fmt.bold(Fmt.error("FAILED")), test.suite_name, test.display_name)
 				_print_failure_report(event.reports())
 			elif event.is_warning():
-				_print_result(_bold(_warning("WARNING")), test.suite_name, test.display_name)
+				_print_result(Fmt.bold(Fmt.warning("WARNING")), test.suite_name, test.display_name)
 				_print_failure_report(event.reports())
 
 
 # *** CONSOLE OUTPUT
 
-static func _colored(text: String, color: Color) -> String:
-	return "[color=%s]%s[/color]" % [color.to_html(), text]
-
-
-static func _bold(text: String) -> String:
-	return "[b]%s[/b]" % text
-
-
-static func _step() -> String: return _bold(_colored("==> ", Color.WHITE))
-static func _substep() -> String: return _bold(_colored("--> ", Color.WHITE))
-static func _prominent(text: String) -> String: return _bold(_colored(text, Color.WHITE))
-
-static func _primary(text: String) -> String: return _colored(text, Color.WHITE)
-static func _accent(text: String) -> String: return _colored(text, Color.DODGER_BLUE)
-static func _muted(text: String) -> String: return _colored(text, Color.GRAY)
-static func _success(text: String) -> String: return _colored(text, Color.GREEN)
-static func _warning(text: String) -> String: return _colored(text, Color.GOLDENROD)
-static func _error(text: String) -> String: return _colored(text, Color.RED)
-
-static func _suite(text: String) -> String: return _colored(text, Color.DARK_CYAN)
-static func _case(text: String) -> String: return _colored(text, Color.CYAN)
-
-
 static func _print_result(status: String, test_suite: String, test_case: String) -> void:
-	print_rich(_substep(), status, ": ",
-			_suite(test_suite), " > ", _case(test_case))
+	print_rich(Fmt.substep(), status, ": ",
+			Fmt.suite(test_suite), " > ", Fmt.case(test_case))
 
 
 static func _print_failure_report(reports: Array[GdUnitReport]) -> void:
@@ -206,11 +183,33 @@ static func _print_stats(p_stats: Stats, p_header: String) -> void:
 	var skipped: String = "  %d skipped" % p_stats.num_skipped
 	var flaky: String = "  %d flaky" % p_stats.num_flaky
 	print_rich(
-		_substep(),
-		_bold(_accent(p_header)), ": ",
-		_primary(total),
-		_error(errors) if p_stats.num_errors > 0 else _primary(errors),
-		_error(failed) if p_stats.num_failed > 0 else _primary(failed),
-		_primary(skipped),
-		_warning(flaky) if p_stats.num_flaky > 0 else _primary(flaky)
+		Fmt.substep(),
+		Fmt.bold(Fmt.accent(p_header)), ": ",
+		Fmt.primary(total),
+		Fmt.error(errors) if p_stats.num_errors > 0 else Fmt.primary(errors),
+		Fmt.error(failed) if p_stats.num_failed > 0 else Fmt.primary(failed),
+		Fmt.primary(skipped),
+		Fmt.warning(flaky) if p_stats.num_flaky > 0 else Fmt.primary(flaky)
 	)
+
+
+class Fmt:
+	static func colored(text: String, color: Color) -> String:
+		return "[color=%s]%s[/color]" % [color.to_html(), text]
+
+	static func bold(text: String) -> String:
+		return "[b]%s[/b]" % text
+
+	static func step() -> String: return bold(colored("==> ", Color.WHITE))
+	static func substep() -> String: return bold(colored("--> ", Color.WHITE))
+	static func prominent(text: String) -> String: return bold(colored(text, Color.WHITE))
+
+	static func primary(text: String) -> String: return colored(text, Color.WHITE)
+	static func accent(text: String) -> String: return colored(text, Color.DODGER_BLUE)
+	static func muted(text: String) -> String: return colored(text, Color.GRAY)
+	static func success(text: String) -> String: return colored(text, Color.GREEN)
+	static func warning(text: String) -> String: return colored(text, Color.GOLDENROD)
+	static func error(text: String) -> String: return colored(text, Color.RED)
+
+	static func suite(text: String) -> String: return colored(text, Color.DARK_CYAN)
+	static func case(text: String) -> String: return colored(text, Color.CYAN)
