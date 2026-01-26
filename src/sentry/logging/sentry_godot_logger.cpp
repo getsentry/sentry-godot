aef0d5e82b227038004b07ba116cba7238be845d
Serhii Snitsaruk <limbonaut@pm.me>
2026-01-26 14:08:43 +0100

    Replace std::hash with FNV-1a hash

--- src/sentry/logging/sentry_godot_logger.cpp
+++ src/sentry/logging/sentry_godot_logger.cpp
@@ -162,17 +162,34 @@ Vector<SentryEvent::StackFrame> _extract_error_stack_frames_from_backtraces(
 	return frames;
 }
 
-template <class T>
-inline size_t _hash(const T &p_value) {
-	std::hash<T> hasher;
-	return hasher(p_value);
+// FNV-1a hash - portable hash implementation that doesn't rely on std::hash<*>.
+// See https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+inline size_t _fnv1a_hash(const char *p_data, size_t p_len) {
+	// FNV-1a constants for 64-bit or 32-bit depending on platform
+	// See table: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV_hash_parameters
+	constexpr size_t FNV_OFFSET_BASIS = sizeof(size_t) == 8 ? 14695981039346656037ULL : 2166136261U;
+	constexpr size_t FNV_PRIME = sizeof(size_t) == 8 ? 1099511628211ULL : 16777619U;
+
+	size_t hash = FNV_OFFSET_BASIS;
+	for (size_t i = 0; i < p_len; i++) {
+		hash ^= static_cast<uint64_t>(static_cast<unsigned char>(p_data[i]));
+		hash *= FNV_PRIME;
+	}
+	return hash;
 }
 
-template <class T>
-inline void _hash_combine(std::size_t &p_hash, const T &p_value) {
+inline size_t _hash(std::string_view p_value) {
+	return _fnv1a_hash(p_value.data(), p_value.size());
+}
+
+inline size_t _hash(int p_value) {
+	return p_value;
+}
+
+template <typename T>
+inline void _hash_combine(std::size_t &p_hash, T p_value) {
 	// NOTE: Hash combining technique, originally from boost.
-	std::hash<T> hasher;
-	p_hash ^= hasher(p_value) + 0x9e3779b9 + (p_hash << 6) + (p_hash >> 2);
+	p_hash ^= _hash(p_value) + 0x9e3779b9 + (p_hash << 6) + (p_hash >> 2);
 }
 
 String _strip_invisible(const String &p_text) {
