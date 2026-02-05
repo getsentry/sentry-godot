0ca37be22dd1894bb1c063c1724338a5a8798df2
Serhii Snitsaruk <limbonaut@pm.me>
2026-02-05 16:30:59 +0100

    Replace C-style casts with Variant operator calls

--- src/sentry/javascript/javascript_util.cpp
+++ src/sentry/javascript/javascript_util.cpp
@@ -24,17 +24,17 @@ String attributes_to_json(const Dictionary &p_attributes) {
 		writer.key(key);
 		switch (value.get_type()) {
 			case Variant::BOOL: {
-				writer.value_bool((bool)value);
+				writer.value_bool(value.operator bool());
 			} break;
 			case Variant::INT: {
-				writer.value_int((int64_t)value);
+				writer.value_int(value.operator int64_t());
 			} break;
 			case Variant::FLOAT: {
-				writer.value_float((double)value);
+				writer.value_float(value.operator double());
 			} break;
 			case Variant::STRING:
 			case Variant::STRING_NAME: {
-				writer.value_string((String)value);
+				writer.value_string(value.operator String());
 			} break;
 			default: {
 				// Stringify other types
