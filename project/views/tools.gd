fa87d75b2f1062be94c92686e9574dca4a5765fb
Serhii Snitsaruk <limbonaut@pm.me>
2026-02-05 06:00:21 -0800

    chore(project): Add isolated test runner in tools panel (#491)

--- project/views/tools.gd
+++ project/views/tools.gd
@@ -2,8 +2,58 @@ extends VBoxContainer
 ## A collection of dev tools accessible through buttons in the demo project.
 
 
+var _isolated_test_files: PackedStringArray
+var _dots_timer: Timer
+var _dot_count: int
+
+
 func _ready() -> void:
 	%RunTestsButton.visible = DirAccess.dir_exists_absolute("res://test/suites/")
+	_setup_isolated_tests_menu()
+
+
+func _setup_isolated_tests_menu() -> void:
+	var menu_button := %IsolatedTestsMenu as MenuButton
+	if not DirAccess.dir_exists_absolute("res://test/isolated/"):
+		menu_button.visible = false
+		return
+
+	_isolated_test_files = PackedStringArray()
+	var dir := DirAccess.open("res://test/isolated/")
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+	while file_name != "":
+		if file_name.begins_with("test_") and file_name.ends_with(".gd"):
+			_isolated_test_files.append(file_name)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+	_isolated_test_files.sort()
+
+	var popup := menu_button.get_popup()
+	for i in _isolated_test_files.size():
+		popup.add_item(_isolated_test_files[i], i)
+	popup.id_pressed.connect(_on_isolated_test_selected)
+
+
+func _start_dots_animation(button: MenuButton) -> void:
+	_dot_count = 0
+	_dots_timer = Timer.new()
+	button.text = "Running."
+	_dots_timer.wait_time = 0.4
+	_dots_timer.timeout.connect(func() -> void:
+		_dot_count = (_dot_count % 3) + 1
+		button.text = "Running" + ".".repeat(_dot_count)
+	)
+	add_child(_dots_timer)
+	_dots_timer.start()
+
+
+func _stop_dots_animation(button: MenuButton) -> void:
+	if _dots_timer:
+		_dots_timer.stop()
+		_dots_timer.queue_free()
+		_dots_timer = null
+	button.text = "Run isolated test..."
 
 
 func _on_run_tests_button_pressed() -> void:
@@ -13,6 +63,26 @@ func _on_run_tests_button_pressed() -> void:
 	await test_runner.finished
 	DemoOutput.print_info("Tests finished with code: %d" % test_runner.result_code)
 	test_runner.queue_free()
+
+
+func _on_isolated_test_selected(id: int) -> void:
+	var menu_button := %IsolatedTestsMenu as MenuButton
+	menu_button.disabled = true
+	_start_dots_animation(menu_button)
+
+	SentrySDK.close()
+	DemoOutput.print_info("Preparing to run isolated test: " + _isolated_test_files[id])
+	await get_tree().create_timer(1.0).timeout
+
+	var test_runner = load("res://test/util/test_runner.gd").new()
+	test_runner.include_tests("res://test/isolated/" + _isolated_test_files[id])
+	add_child(test_runner)
+	await test_runner.finished
+	DemoOutput.print_info("Isolated test finished with code: %d" % test_runner.result_code)
+	test_runner.queue_free()
+
+	_stop_dots_animation(menu_button)
+	menu_button.disabled = false
 
 
 func _on_test_diverse_context_button_pressed() -> void:
