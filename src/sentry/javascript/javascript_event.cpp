4f185a1eaa255545ad922817b3cba68c74993cbe
Serhii Snitsaruk <limbonaut@pm.me>
2026-02-20 14:00:48 +0100

    Adapt JavaScriptEvent

--- src/sentry/javascript/javascript_event.cpp
+++ src/sentry/javascript/javascript_event.cpp
@@ -1,12 +1,8 @@
 #include "javascript_event.h"
 
-#include "javascript_string_names.h"
-#include "javascript_util.h"
 #include "sentry/util/json_writer.h"
 #include "sentry/uuid.h"
 
-#include <godot_cpp/classes/java_script_bridge.hpp>
-#include <godot_cpp/classes/java_script_object.hpp>
 #include <godot_cpp/classes/json.hpp>
 #include <godot_cpp/classes/os.hpp>
 #include <godot_cpp/classes/time.hpp>
@@ -57,48 +53,50 @@ void write_stacktrace_json(sentry::util::JSONWriter &p_jw, const Vector<SentryEv
 } // namespace
 
 String JavaScriptEvent::get_id() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(event_id));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get("event_id").as_string();
 }
 
 void JavaScriptEvent::set_message(const String &p_message) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(message), p_message);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("message", p_message.utf8());
 }
 
 String JavaScriptEvent::get_message() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
+	ERR_FAIL_COND_V(!js_obj, String());
 
-	Variant message_value = js_obj->get(JAVASCRIPT_SN(message));
+	JSValue message_value = js_obj->get("message");
 
-	if (message_value == Variant()) {
+	if (message_value.get_type() == JSValueType::NIL) {
 		return String();
 	}
 
-	if (message_value.get_type() == Variant::STRING) {
-		return message_value.operator String();
+	if (message_value.get_type() == JSValueType::STRING) {
+		return message_value.as_string();
 	}
 
-	Ref<JavaScriptObject> jso = message_value;
-	if (jso.is_valid()) {
-		return js_object_get_property_as_string(jso, JAVASCRIPT_SN(formatted));
+	JSObjectPtr jso = message_value.as_object();
+	if (jso) {
+		return jso->get_as_string("formatted");
 	}
 
 	return String();
 }
 
 void JavaScriptEvent::set_timestamp(const Ref<SentryTimestamp> &p_timestamp) {
-	ERR_FAIL_COND(js_obj.is_null());
+	ERR_FAIL_COND(!js_obj);
+
 	if (p_timestamp.is_valid()) {
-		js_object_set_double(js_obj, JAVASCRIPT_SN(timestamp), p_timestamp->to_unix_time());
+		js_obj->set("timestamp", p_timestamp->to_unix_time());
 	} else {
-		js_delete_property(js_obj, JAVASCRIPT_SN(timestamp));
+		js_obj->delete_property("timestamp");
 	}
 }
 
 Ref<SentryTimestamp> JavaScriptEvent::get_timestamp() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), Ref<SentryTimestamp>());
-	double unix_time = js_object_get_double(js_obj, JAVASCRIPT_SN(timestamp), 0.0);
+	ERR_FAIL_COND_V(!js_obj, Ref<SentryTimestamp>());
+
+	double unix_time = js_obj->get("timestamp").as_double();
 	if (unix_time == 0.0) {
 		return Ref<SentryTimestamp>();
 	}
@@ -106,93 +104,93 @@ Ref<SentryTimestamp> JavaScriptEvent::get_timestamp() const {
 }
 
 String JavaScriptEvent::get_platform() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(platform));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get("platform").as_string();
 }
 
 void JavaScriptEvent::set_level(sentry::Level p_level) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_obj->set(JAVASCRIPT_SN(level), level_as_string(p_level));
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set("level", level_as_cstring(p_level));
 }
 
 sentry::Level JavaScriptEvent::get_level() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), sentry::Level::LEVEL_ERROR);
-	String level_str = js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(level));
+	ERR_FAIL_COND_V(!js_obj, sentry::Level::LEVEL_ERROR);
+	String level_str = js_obj->get("level").as_string();
 	return sentry::level_from_string(level_str, sentry::Level::LEVEL_ERROR);
 }
 
 void JavaScriptEvent::set_logger(const String &p_logger) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(logger), p_logger);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("logger", p_logger.utf8());
 }
 
 String JavaScriptEvent::get_logger() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(logger));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get("logger").as_string();
 }
 
 void JavaScriptEvent::set_release(const String &p_release) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(release), p_release);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("release", p_release.utf8());
 }
 
 String JavaScriptEvent::get_release() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(release));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get("release").as_string();
 }
 
 void JavaScriptEvent::set_dist(const String &p_dist) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(dist), p_dist);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("dist", p_dist.utf8());
 }
 
 String JavaScriptEvent::get_dist() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(dist));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get("dist").as_string();
 }
 
 void JavaScriptEvent::set_environment(const String &p_environment) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(environment), p_environment);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("environment", p_environment.utf8());
 }
 
 String JavaScriptEvent::get_environment() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(environment));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get("environment").as_string();
 }
 
 void JavaScriptEvent::set_tag(const String &p_key, const String &p_value) {
-	ERR_FAIL_COND(js_obj.is_null());
-	Ref<JavaScriptObject> tags_obj = js_object_get_or_create_object_property(js_obj, JAVASCRIPT_SN(tags));
-	tags_obj->set(p_key, p_value);
+	ERR_FAIL_COND(!js_obj);
+	JSObjectPtr tags_obj = js_obj->get_or_create_object_property("tags");
+	tags_obj->set(p_key.utf8(), p_value.utf8());
 }
 
 void JavaScriptEvent::remove_tag(const String &p_key) {
-	ERR_FAIL_COND(js_obj.is_null());
-	Ref<JavaScriptObject> tags_obj = js_obj->get(JAVASCRIPT_SN(tags));
-	if (tags_obj.is_valid()) {
-		js_delete_property(tags_obj, p_key);
+	ERR_FAIL_COND(!js_obj);
+	JSObjectPtr tags_obj = js_obj->get_or_create_object_property("tags");
+	if (tags_obj) {
+		tags_obj->delete_property(p_key.utf8());
 	}
 }
 
 String JavaScriptEvent::get_tag(const String &p_key) {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	Ref<JavaScriptObject> tags_obj = js_obj->get(JAVASCRIPT_SN(tags));
-	if (tags_obj.is_valid()) {
-		return js_object_get_property_as_string(tags_obj, p_key);
+	ERR_FAIL_COND_V(!js_obj, String());
+	JSObjectPtr tags_obj = js_obj->get_or_create_object_property("tags");
+	if (tags_obj) {
+		return tags_obj->get(p_key.utf8()).as_string();
 	}
 	return String();
 }
 
 void JavaScriptEvent::merge_context(const String &p_key, const Dictionary &p_value) {
-	ERR_FAIL_COND(js_obj.is_null());
-	Ref<JavaScriptObject> contexts_obj = js_object_get_or_create_object_property(js_obj, JAVASCRIPT_SN(contexts));
-	Ref<JavaScriptObject> context_obj = js_object_get_or_create_object_property(contexts_obj, p_key);
-	js_merge_json_into_object(context_obj, JSON::stringify(p_value));
+	ERR_FAIL_COND(!js_obj);
+	JSObjectPtr contexts_obj = js_obj->get_or_create_object_property("contexts");
+	JSObjectPtr context_obj = contexts_obj->get_or_create_object_property(p_key.utf8());
+	context_obj->merge_properties_from_json(JSON::stringify(p_value).utf8());
 }
 
 void JavaScriptEvent::add_exception(const Exception &p_exception) {
-	ERR_FAIL_COND(js_obj.is_null());
+	ERR_FAIL_COND(!js_obj);
 
 	uint64_t thread_id = OS::get_singleton()->get_thread_caller_id();
 	bool is_main = OS::get_singleton()->get_main_thread_id() == thread_id;
@@ -208,9 +206,9 @@ void JavaScriptEvent::add_exception(const Exception &p_exception) {
 		write_stacktrace_json(jw, p_exception.frames);
 		jw.end_object(); // } thread
 
-		Ref<JavaScriptObject> threads_obj = js_object_get_or_create_object_property(js_obj, JAVASCRIPT_SN(threads));
-		Ref<JavaScriptObject> threads_arr = js_object_get_or_create_array_property(threads_obj, JAVASCRIPT_SN(values));
-		js_push_json_to_array(threads_arr, jw.get_string());
+		JSObjectPtr threads_obj = js_obj->get_or_create_object_property("threads");
+		JSObjectPtr threads_arr = threads_obj->get_or_create_array_property("values");
+		threads_arr->push_element_from_json(jw.get_string().utf8());
 	}
 
 	// Create exception with thread_id reference.
@@ -222,77 +220,77 @@ void JavaScriptEvent::add_exception(const Exception &p_exception) {
 	exc_jw.kv_int("thread_id", thread_id);
 	exc_jw.end_object(); // } exception
 
-	Ref<JavaScriptObject> exception_obj = js_object_get_or_create_object_property(js_obj, JAVASCRIPT_SN(exception));
-	Ref<JavaScriptObject> values_arr = js_object_get_or_create_array_property(exception_obj, JAVASCRIPT_SN(values));
-	js_push_json_to_array(values_arr, exc_jw.get_string());
+	JSObjectPtr exception_obj = js_obj->get_or_create_object_property("exception");
+	JSObjectPtr values_arr = exception_obj->get_or_create_array_property("values");
+	values_arr->push_element_from_json(exc_jw.get_string().utf8());
 }
 
 int JavaScriptEvent::get_exception_count() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), 0);
-	Ref<JavaScriptObject> exception_obj = js_obj->get(JAVASCRIPT_SN(exception));
-	if (exception_obj.is_valid()) {
-		Ref<JavaScriptObject> values_arr = exception_obj->get(JAVASCRIPT_SN(values));
-		if (values_arr.is_valid()) {
-			return values_arr->get(JAVASCRIPT_SN(length));
+	ERR_FAIL_COND_V(!js_obj, 0);
+	JSObjectPtr exception_obj = js_obj->get("exception").as_object();
+	if (exception_obj) {
+		JSObjectPtr values_arr = exception_obj->get("values").as_object();
+		if (values_arr) {
+			return values_arr->get("length").as_int();
 		}
 	}
 	return 0;
 }
 
 void JavaScriptEvent::set_exception_value(int p_index, const String &p_value) {
-	ERR_FAIL_COND(js_obj.is_null());
+	ERR_FAIL_COND(!js_obj);
 
-	Ref<JavaScriptObject> exception_obj = js_obj->get(JAVASCRIPT_SN(exception));
-	if (exception_obj.is_null()) {
+	JSObjectPtr exception_obj = js_obj->get("exception").as_object();
+	if (!exception_obj) {
 		WARN_PRINT("Sentry: Exception data not found.");
 		return;
 	}
 
-	Ref<JavaScriptObject> values_arr = exception_obj->get(JAVASCRIPT_SN(values));
-	if (values_arr.is_null()) {
+	JSObjectPtr values_arr = exception_obj->get("values").as_object();
+	if (!values_arr) {
 		WARN_PRINT("Sentry: Exception values not found.");
 		return;
 	}
 
-	int length = values_arr->get(JAVASCRIPT_SN(length));
+	int64_t length = values_arr->get("length").as_int();
 	if (p_index < 0 || p_index >= length) {
 		WARN_PRINT("Sentry: Exception with index " + itos(p_index) + " not found.");
 		return;
 	}
 
-	Ref<JavaScriptObject> exc_obj = values_arr->get(String::num_int64(p_index));
-	if (exc_obj.is_null()) {
+	JSObjectPtr exc_obj = values_arr->get(String::num_int64(p_index).ascii()).as_object();
+	if (!exc_obj) {
 		WARN_PRINT("Sentry: Expected exception object.");
 		return;
 	}
 
-	exc_obj->set(JAVASCRIPT_SN(value), p_value);
+	exc_obj->set("value", p_value.utf8());
 }
 
 String JavaScriptEvent::get_exception_value(int p_index) const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
+	ERR_FAIL_COND_V(!js_obj, String());
 
-	Ref<JavaScriptObject> exception_obj = js_obj->get(JAVASCRIPT_SN(exception));
-	if (exception_obj.is_null()) {
+	JSObjectPtr exception_obj = js_obj->get("exception").as_object();
+	if (!exception_obj) {
 		return String();
 	}
 
-	Ref<JavaScriptObject> values_arr = exception_obj->get(JAVASCRIPT_SN(values));
-	if (values_arr.is_null()) {
+	JSObjectPtr values_arr = exception_obj->get("values").as_object();
+	if (!values_arr) {
 		return String();
 	}
 
-	int length = values_arr->get(JAVASCRIPT_SN(length));
+	int64_t length = values_arr->get("length").as_int();
 	if (p_index < 0 || p_index >= length) {
 		return String();
 	}
 
-	Ref<JavaScriptObject> exc_obj = values_arr->get(String::num_int64(p_index));
-	if (exc_obj.is_null()) {
+	JSObjectPtr exc_obj = values_arr->get(String::num_int64(p_index).ascii()).as_object();
+	if (!exc_obj) {
 		return String();
 	}
 
-	return js_object_get_property_as_string(exc_obj, JAVASCRIPT_SN(value));
+	return exc_obj->get_as_string("value");
 }
 
 bool JavaScriptEvent::is_crash() const {
@@ -300,23 +298,22 @@ bool JavaScriptEvent::is_crash() const {
 }
 
 String JavaScriptEvent::to_json() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_to_json(js_obj);
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->to_json();
 }
 
-JavaScriptEvent::JavaScriptEvent(const Ref<RefCounted> &p_js_event_object) {
+JavaScriptEvent::JavaScriptEvent(const JSObjectPtr &p_js_event_object) {
 	js_obj = p_js_event_object;
 }
 
 JavaScriptEvent::JavaScriptEvent() {
-	js_obj = JavaScriptBridge::get_singleton()->create_object(JAVASCRIPT_SN(Object));
+	js_obj = JSObject::create("Object");
 
 	// Capture current timestamp
-	js_object_set_double(js_obj, JAVASCRIPT_SN(timestamp),
-			Time::get_singleton()->get_unix_time_from_system());
+	js_obj->set("timestamp", Time::get_singleton()->get_unix_time_from_system());
 
 	// Pre-generate event-id
-	js_obj->set(JAVASCRIPT_SN(event_id), sentry::uuid::make_uuid_no_dashes());
+	js_obj->set("event_id", sentry::uuid::make_uuid_no_dashes().ascii());
 }
 
 JavaScriptEvent::~JavaScriptEvent() {
