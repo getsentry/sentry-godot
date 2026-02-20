a7962359ef0c6e2613a6d7464984a61f81c1eb54
Serhii Snitsaruk <limbonaut@pm.me>
2026-02-20 13:23:32 +0100

    Adapt JavaScriptBreadscrumb

--- src/sentry/javascript/javascript_breadcrumb.cpp
+++ src/sentry/javascript/javascript_breadcrumb.cpp
@@ -1,75 +1,82 @@
 #include "javascript_breadcrumb.h"
 
-#include "javascript_string_names.h"
-#include "javascript_util.h"
+#include "sentry/logging/print.h"
 
-#include <godot_cpp/classes/java_script_bridge.hpp>
-#include <godot_cpp/classes/java_script_object.hpp>
 #include <godot_cpp/classes/json.hpp>
 #include <godot_cpp/classes/time.hpp>
 
 namespace sentry::javascript {
 
 void JavaScriptBreadcrumb::set_message(const String &p_message) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(message), p_message);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("message", p_message.utf8());
 }
 
 String JavaScriptBreadcrumb::get_message() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(message));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get_as_string("message");
 }
 
 void JavaScriptBreadcrumb::set_category(const String &p_category) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(category), p_category);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("category", p_category.utf8());
 }
 
 String JavaScriptBreadcrumb::get_category() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(category));
+	ERR_FAIL_COND_V(!js_obj, String());
+	return js_obj->get_as_string("category");
 }
 
 void JavaScriptBreadcrumb::set_level(sentry::Level p_level) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_obj->set(JAVASCRIPT_SN(level), level_as_string(p_level));
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set("level", level_as_cstring(p_level));
 }
 
 sentry::Level JavaScriptBreadcrumb::get_level() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), sentry::Level::LEVEL_INFO);
+	ERR_FAIL_COND_V(!js_obj, sentry::Level::LEVEL_INFO);
 
-	Variant value = js_obj->get(JAVASCRIPT_SN(level));
-	if (value == Variant()) {
-		return sentry::Level::LEVEL_INFO;
+	JSValue value = js_obj->get("level");
+	switch (value.get_type()) {
+		case JSValueType::NIL: {
+			return sentry::Level::LEVEL_INFO;
+		} break;
+		case JSValueType::STRING: {
+			return level_from_string(value.as_string(), sentry::Level::LEVEL_INFO);
+		} break;
+		default: {
+			sentry::logging::print_error("Breadcrumb has unexpected level value.");
+			return sentry::Level::LEVEL_INFO;
+		} break;
 	}
-
-	return sentry::level_from_string(value, sentry::Level::LEVEL_INFO);
 }
 
 void JavaScriptBreadcrumb::set_type(const String &p_type) {
-	ERR_FAIL_COND(js_obj.is_null());
-	js_object_set_or_remove_string_property(js_obj, JAVASCRIPT_SN(type), p_type);
+	ERR_FAIL_COND(!js_obj);
+	js_obj->set_or_remove_string_property("type", p_type.utf8());
 }
 
 String JavaScriptBreadcrumb::get_type() const {
-	ERR_FAIL_COND_V(js_obj.is_null(), String());
-	return js_object_get_property_as_string(js_obj, JAVASCRIPT_SN(type));
+	ERR_FAIL_COND_V(!js_obj, "");
+
+	return js_obj->get_as_string("type");
 }
 
 void JavaScriptBreadcrumb::set_data(const Dictionary &p_data) {
-	ERR_FAIL_COND(js_obj.is_null());
+	ERR_FAIL_COND(!js_obj);
+
 	if (!p_data.is_empty()) {
-		js_delete_property(js_obj, JAVASCRIPT_SN(data));
-		Ref<JavaScriptObject> data_obj = js_object_get_or_create_object_property(js_obj, JAVASCRIPT_SN(data));
-		js_merge_json_into_object(data_obj, JSON::stringify(p_data));
+		JSObjectPtr data_obj = JSObject::create("Object");
+		data_obj->merge_properties_from_json(JSON::stringify(p_data).utf8());
+		js_obj->set("data", data_obj);
 	} else {
-		js_delete_property(js_obj, JAVASCRIPT_SN(data));
+		js_obj->delete_property("data");
 	}
 }
 
 Ref<SentryTimestamp> JavaScriptBreadcrumb::get_timestamp() {
-	ERR_FAIL_COND_V(js_obj.is_null(), Ref<SentryTimestamp>());
-	double unix_time = js_object_get_double(js_obj, JAVASCRIPT_SN(timestamp), 0.0);
+	ERR_FAIL_COND_V(!js_obj, Ref<SentryTimestamp>());
+
+	double unix_time = js_obj->get("timestamp").as_double();
 	if (unix_time == 0.0) {
 		return Ref<SentryTimestamp>();
 	}
@@ -77,11 +84,10 @@ Ref<SentryTimestamp> JavaScriptBreadcrumb::get_timestamp() {
 }
 
 JavaScriptBreadcrumb::JavaScriptBreadcrumb() {
-	js_obj = JavaScriptBridge::get_singleton()->create_object(JAVASCRIPT_SN(Object));
+	js_obj = JSObject::create("Object");
 
 	// Capture current timestamp
-	js_object_set_double(js_obj, JAVASCRIPT_SN(timestamp),
-			Time::get_singleton()->get_unix_time_from_system());
+	js_obj->set("timestamp", Time::get_singleton()->get_unix_time_from_system());
 }
 
 JavaScriptBreadcrumb::~JavaScriptBreadcrumb() {
