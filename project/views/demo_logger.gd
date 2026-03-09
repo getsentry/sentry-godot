f04e40613ad2e93c2b9afc74dddfbfe51e01bd7f
Serhii Snitsaruk <limbonaut@pm.me>
2026-03-09 21:44:45 +0100

    Remove ANSI escape codes from the output

--- project/views/demo_logger.gd
+++ project/views/demo_logger.gd
@@ -5,6 +5,9 @@ signal message_logged(text: String)
 
 var message_pool: PackedStringArray
 
+var _ansi_escape_regex: RegEx = RegEx.create_from_string(r"\x1b\[[0-9;]*[a-zA-Z]")
+
 func _log_message(message: String, _error: bool) -> void:
+	message = _ansi_escape_regex.sub(message, "", true)
 	message_pool.append(message)
 	message_logged.emit.call_deferred(message)
