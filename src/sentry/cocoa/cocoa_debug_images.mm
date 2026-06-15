1c9a8467f9aea2f3820698f8cd2468d75ba84ef4
Serhii Snitsaruk <limbonaut@pm.me>
2026-06-15 21:06:15 +0200

    Add Cocoa debug image lookup workaround

--- src/sentry/cocoa/cocoa_debug_images.mm
+++ src/sentry/cocoa/cocoa_debug_images.mm
@@ -2,41 +2,57 @@
 
 #include "cocoa_includes.h"
 
+// WORKAROUND: The SentryObjC SDK has no public or internal API to look up binary images by address,
+// which we need to symbolicate .NET exceptions.
+// The types that do this are still inside the framework binary, just not declared in any header.
+// We redeclare the few bits we call here so the code compiles and links against them.
+// Remove once the SDK exposes an official API.
+// Upstream issue: https://github.com/getsentry/sentry-cocoa/issues/8039
+
+@interface SentryBinaryImageInfo : NSObject
+@property(nonatomic, readonly) NSString *name;
+@property(nonatomic, readonly) NSString *uuid;
+@property(nonatomic, readonly) uint64_t address;
+@property(nonatomic, readonly) uint64_t size;
+@end
+
+@interface SentryBinaryImageCache : NSObject
+- (SentryBinaryImageInfo *)imageByAddress:(uint64_t)address;
+@end
+
+@interface SentryDependencies : NSObject
+@property(class, nonatomic, readonly) SentryBinaryImageCache *binaryImageCache;
+@end
+
+// END OF WORKAROUND
+
 namespace sentry::cocoa {
 
 Vector<DebugImage> get_debug_images(const int64_t *p_addresses, int32_t p_addresses_count) {
-	// GAP: SentryObjC wrapper SDK doesn't expose SentryDependencies.binaryImageCache / SentryBinaryImageInfo,
-	//      which were used to resolve debug images for .NET exception symbolication.
-	//      In the main Cocoa package, these are internal SPI classes (the upstream .NET SDK consumes these in a
-	//      similar way as we do in Godot).
-	// TODO: Needs upstream exposure.
-	WARN_PRINT_ONCE("Sentry: Debug image resolution is currently not supported with the SentryObjC SDK.");
-	return Vector<DebugImage>();
+	Vector<DebugImage> images;
+	if (p_addresses == nullptr || p_addresses_count <= 0) {
+		return images;
+	}
 
-	// Vector<DebugImage> images;
-	// if (p_addresses == nullptr || p_addresses_count <= 0) {
-	// 	return images;
-	// }
+	SentryBinaryImageCache *cache = SentryDependencies.binaryImageCache;
+	if (cache == nil) {
+		return images;
+	}
 
-	// SentryBinaryImageCache *cache = SentryDependencies.binaryImageCache;
-	// if (cache == nil) {
-	// 	return images;
-	// }
+	for (int32_t i = 0; i < p_addresses_count; ++i) {
+		SentryBinaryImageInfo *info = [cache imageByAddress:(uint64_t)p_addresses[i]];
+		if (info == nil) {
+			continue;
+		}
 
-	// for (int32_t i = 0; i < p_addresses_count; ++i) {
-	// 	SentryBinaryImageInfo *info = [cache imageByAddress:(uint64_t)p_addresses[i]];
-	// 	if (info == nil) {
-	// 		continue;
-	// 	}
-
-	// 	DebugImage image;
-	// 	image.code_file = String::utf8([info.name UTF8String]); // info.name is nonnull
-	// 	image.debug_id = info.uuid ? String::utf8([info.uuid UTF8String]) : String();
-	// 	image.image_address = static_cast<int64_t>(info.address);
-	// 	image.image_size = static_cast<int64_t>(info.size);
-	// 	images.push_back(image);
-	// }
-	// return images;
+		DebugImage image;
+		image.code_file = String::utf8([info.name UTF8String]); // info.name is nonnull
+		image.debug_id = info.uuid ? String::utf8([info.uuid UTF8String]) : String();
+		image.image_address = static_cast<int64_t>(info.address);
+		image.image_size = static_cast<int64_t>(info.size);
+		images.push_back(image);
+	}
+	return images;
 }
 
 } // namespace sentry::cocoa
