afa306c551e8fc0a8dda3243120a389a7de4a3ad
Serhii Snitsaruk <limbonaut@pm.me>
2026-06-16 11:48:51 +0200

    Use GDExtension-supplied path on other platforms

--- src/sentry/util/library_path.cpp
+++ src/sentry/util/library_path.cpp
@@ -5,20 +5,17 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
-#else // !_WIN32
-#include <dlfcn.h>
 #endif
 
 namespace sentry::util {
 
 godot::String get_loaded_gdextension_library_path() {
-	// NOTE: In the Windows editor, Godot loads a temporary "~"-prefixed copy of the library,
+#if defined(_WIN32)
+	// In the Windows editor, Godot loads a temporary "~"-prefixed copy of the library,
 	// while gdextension_interface_get_library_path() reports the original path.
 	// Passing that path to C# DllImport resolver would load a second, uninitialized copy of the library
 	// (with a null SentrySDK singleton), so resolve the module that is actually loaded instead.
 	// See issue 761.
-
-#if defined(_WIN32)
 	HMODULE module = nullptr;
 	if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
 				reinterpret_cast<LPCWSTR>(&get_loaded_gdextension_library_path),
@@ -32,11 +29,9 @@ godot::String get_loaded_gdextension_library_path() {
 	}
 	return godot::String();
 #else
-	Dl_info info = {};
-	if (dladdr(reinterpret_cast<const void *>(&get_loaded_gdextension_library_path), &info) != 0 && info.dli_fname != nullptr) {
-		return godot::String::utf8(info.dli_fname);
-	}
-	return godot::String();
+	godot::String library_path;
+	godot::internal::gdextension_interface_get_library_path(godot::internal::library, library_path._native_ptr());
+	return library_path;
 #endif
 }
 
