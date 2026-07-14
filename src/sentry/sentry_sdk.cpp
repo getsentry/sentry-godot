2b5006ebd95e3e5a12814807a2b4f2e47f0a6575
Serhii Snitsaruk <limbonaut@pm.me>
2026-07-14 16:53:25 +0200

    Just the logger

--- src/sentry/sentry_sdk.cpp
+++ src/sentry/sentry_sdk.cpp
@@ -516,7 +516,10 @@ void SentrySDK::_notification(int p_what) {
 			sentry::engine_lifecycle::remove_shutdown_callback(
 					callable_mp(this, &SentrySDK::_on_engine_shutdown));
 			// Fallback in case _on_engine_shutdown() did not run.
-			close();
+			if (godot_logger.is_valid()) {
+				OS::get_singleton()->remove_logger(godot_logger);
+				godot_logger.unref();
+			}
 		} break;
 	}
 }
