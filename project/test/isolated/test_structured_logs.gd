ad7ef1e6abdb047527fc724781bdd67d86692636
Serhii Snitsaruk <limbonaut@pm.me>
2026-07-22 10:26:34 +0200

    Add skip note about JS scope attribute merge

--- project/test/isolated/test_structured_logs.gd
+++ project/test/isolated/test_structured_logs.gd
@@ -199,6 +199,7 @@ func test_structured_logs_with_global_attributes(_do_skip = OS.get_name() == "We
 
 
 # TODO: remove skip when implemented on other platforms
+# Skipped: JS merges scope attributes at serialization time, after the callback has already run.
 func test_structured_logs_with_scope_attributes(_do_skip = OS.get_name() not in ["Windows", "Linux", "Android"]) -> void:
 	SentrySDK.set_attribute("from_global", "global")
 	SentrySDK.set_attribute("scope_over_global", "global")
