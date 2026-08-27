8514380e15e9793eff7fc2d4c69736d7d890a71c
Serhii Snitsaruk <limbonaut@pm.me>
2026-08-27 12:58:52 +0200

    Reorder JavaScript span methods

--- src/sentry/javascript/javascript_span.cpp
+++ src/sentry/javascript/javascript_span.cpp
@@ -8,6 +8,17 @@ namespace sentry::javascript {
 
 // NOTE: Bridge and js_obj are guaranteed to be valid: creation/bridge failures yield a DisabledSpan instead.
 
+SentrySpanImpl *JavaScriptSpan::start_span(const String &p_name, const Dictionary &p_attributes, const JSObjectPtr &p_parent) {
+	String attr_value = attributes_to_json(p_attributes);
+	JSObjectPtr span_obj = js_bridge()->call("startSpan", p_name.utf8(), attr_value.utf8(), p_parent).as_object();
+	ERR_FAIL_COND_V_MSG(!span_obj, SentrySpanImpl::create_noop(), "Sentry: Failed to create span object.");
+	return memnew(JavaScriptSpan(span_obj));
+}
+
+SentrySpanImpl *JavaScriptSpan::start_child(const String &p_name, const Dictionary &p_attributes) {
+	return start_span(p_name, p_attributes, js_obj);
+}
+
 void JavaScriptSpan::set_attribute(const String &p_key, const Variant &p_value) {
 	switch (p_value.get_type()) {
 		case Variant::Type::BOOL: {
@@ -34,17 +45,6 @@ void JavaScriptSpan::set_status(SpanStatus p_status) {
 
 void JavaScriptSpan::end() {
 	js_obj->call("end");
-}
-
-SentrySpanImpl *JavaScriptSpan::start_span(const String &p_name, const Dictionary &p_attributes, const JSObjectPtr &p_parent) {
-	String attr_value = attributes_to_json(p_attributes);
-	JSObjectPtr span_obj = js_bridge()->call("startSpan", p_name.utf8(), attr_value.utf8(), p_parent).as_object();
-	ERR_FAIL_COND_V_MSG(!span_obj, SentrySpanImpl::create_noop(), "Sentry: Failed to create span object.");
-	return memnew(JavaScriptSpan(span_obj));
-}
-
-SentrySpanImpl *JavaScriptSpan::start_child(const String &p_name, const Dictionary &p_attributes) {
-	return start_span(p_name, p_attributes, js_obj);
 }
 
 JavaScriptSpan::JavaScriptSpan(const JSObjectPtr &p_js_span_object) :
