2b00f51ad019920820acaaf0e1b0b1dc22cfc2df
Serhii Snitsaruk <limbonaut@pm.me>
2026-08-28 13:42:52 +0200

    Raise minimum macOS version to 12

---
+++ src/editor/sentry_editor_export_plugin_macos.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifdef TOOLS_ENABLED
+
+#include <godot_cpp/classes/editor_export_platform.hpp>
+#include <godot_cpp/classes/editor_export_plugin.hpp>
+
+using namespace godot;
+
+class SentryEditorExportPluginMacOS : public EditorExportPlugin {
+	GDCLASS(SentryEditorExportPluginMacOS, EditorExportPlugin);
+
+private:
+	bool is_macos = false;
+
+protected:
+	static void _bind_methods() {}
+
+public:
+	virtual String _get_name() const override;
+	virtual bool _supports_platform(const Ref<EditorExportPlatform> &p_platform) const override;
+	virtual TypedArray<Dictionary> _get_export_options(const Ref<EditorExportPlatform> &p_platform) const override;
+	virtual String _get_export_option_warning(const Ref<EditorExportPlatform> &p_platform, const String &p_option) const override;
+	virtual void _export_begin(const PackedStringArray &p_features, bool p_is_debug, const String &p_path, uint32_t p_flags) override;
+	virtual void _export_end() override;
+
+	SentryEditorExportPluginMacOS() = default;
+};
+
+#endif // TOOLS_ENABLED
