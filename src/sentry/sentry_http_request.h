012519e924c236847c5c04a4809d91790df4d5ed
Serhii Snitsaruk <limbonaut@pm.me>
2026-09-16 20:41:29 +0200

    Inline HTTPRequest forwarding methods

--- src/sentry/sentry_http_request.h
+++ src/sentry/sentry_http_request.h
@@ -62,27 +62,27 @@ public:
 	Error request(const String &p_url, const PackedStringArray &p_custom_headers = PackedStringArray(), HTTPClient::Method p_method = HTTPClient::METHOD_GET, const String &p_request_data = String());
 	Error request_raw(const String &p_url, const PackedStringArray &p_custom_headers = PackedStringArray(), HTTPClient::Method p_method = HTTPClient::METHOD_GET, const PackedByteArray &p_request_data_raw = PackedByteArray());
 	void cancel_request();
-	void set_tls_options(const Ref<TLSOptions> &p_client_options);
-	HTTPClient::Status get_http_client_status() const;
 
-	void set_use_threads(bool p_enable);
-	bool is_using_threads() const;
-	void set_accept_gzip(bool p_enable);
-	bool is_accepting_gzip() const;
-	void set_body_size_limit(int32_t p_bytes);
-	int32_t get_body_size_limit() const;
-	void set_max_redirects(int32_t p_amount);
-	int32_t get_max_redirects() const;
-	void set_download_file(const String &p_path);
-	String get_download_file() const;
-	int32_t get_downloaded_bytes() const;
-	int32_t get_body_size() const;
-	void set_timeout(double p_timeout);
-	double get_timeout();
-	void set_download_chunk_size(int32_t p_chunk_size);
-	int32_t get_download_chunk_size() const;
-	void set_http_proxy(const String &p_host, int32_t p_port);
-	void set_https_proxy(const String &p_host, int32_t p_port);
+	void set_tls_options(const Ref<TLSOptions> &p_client_options) { _http_request->set_tls_options(p_client_options); }
+	HTTPClient::Status get_http_client_status() const { return _http_request->get_http_client_status(); }
+	void set_use_threads(bool p_enable) { _http_request->set_use_threads(p_enable); }
+	bool is_using_threads() const { return _http_request->is_using_threads(); }
+	void set_accept_gzip(bool p_enable) { _http_request->set_accept_gzip(p_enable); }
+	bool is_accepting_gzip() const { return _http_request->is_accepting_gzip(); }
+	void set_body_size_limit(int32_t p_bytes) { _http_request->set_body_size_limit(p_bytes); }
+	int32_t get_body_size_limit() const { return _http_request->get_body_size_limit(); }
+	void set_max_redirects(int32_t p_amount) { _http_request->set_max_redirects(p_amount); }
+	int32_t get_max_redirects() const { return _http_request->get_max_redirects(); }
+	void set_download_file(const String &p_path) { _http_request->set_download_file(p_path); }
+	String get_download_file() const { return _http_request->get_download_file(); }
+	int32_t get_downloaded_bytes() const { return _http_request->get_downloaded_bytes(); }
+	int32_t get_body_size() const { return _http_request->get_body_size(); }
+	void set_timeout(double p_timeout) { _http_request->set_timeout(p_timeout); }
+	double get_timeout() { return _http_request->get_timeout(); }
+	void set_download_chunk_size(int32_t p_chunk_size) { _http_request->set_download_chunk_size(p_chunk_size); }
+	int32_t get_download_chunk_size() const { return _http_request->get_download_chunk_size(); }
+	void set_http_proxy(const String &p_host, int32_t p_port) { _http_request->set_http_proxy(p_host, p_port); }
+	void set_https_proxy(const String &p_host, int32_t p_port) { _http_request->set_https_proxy(p_host, p_port); }
 
 	SentryHTTPRequest();
 };
